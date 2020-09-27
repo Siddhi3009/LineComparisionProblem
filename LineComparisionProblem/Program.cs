223d658d95a454804783742c77007ebebe7b0217
Administrator <siddhiseth3009@gmail.com>
2020-09-27 12:42:01 +0530

    [Siddhi]Refactor Line Equality Check added

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -11,7 +11,16 @@ namespace LineComparisionProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation Program");
-            Console.WriteLine("Length of line: " + LengthOfLine());
+            Console.WriteLine("Enter coordinates of Line1");
+            double lengthOfLine1 = LengthOfLine();
+            Console.WriteLine("Length of line 1: " + lengthOfLine1);
+            Console.WriteLine("Enter coordinates of Line2");
+            double lengthOfLine2 = LengthOfLine();
+            Console.WriteLine("Length of line 2: " + lengthOfLine2);
+            if (lengthOfLine1 == lengthOfLine2)
+                Console.WriteLine("Both the lines are equal");
+            else
+                Console.WriteLine("Both the lines are unequal");
         }
         static double LengthOfLine()
         {
