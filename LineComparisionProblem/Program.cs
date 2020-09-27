dcd59afec19579f6391324b0b7c582e5ca4d007d
Administrator <siddhiseth3009@gmail.com>
2020-09-27 12:05:51 +0530

    [Siddhi]Refactor Length Of Line compuatation added

--- LineComparisionProblem/Program.cs
+++ LineComparisionProblem/Program.cs
@@ -11,6 +11,18 @@ namespace LineComparisionProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparision Computation Program");
+            Console.WriteLine("Length of line: " + LengthOfLine());
+        }
+        static double LengthOfLine()
+        {
+            Console.WriteLine("Enter the X & Y coordinates of starting point of the line");
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the X & Y coordinates of ending point of the line");
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            return length;
         }
     }
 }
