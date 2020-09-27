bb12ba1c210bec7b46ccf4597e4910a879f56249
Administrator <siddhiseth3009@gmail.com>
2020-09-27 11:59:32 +0530

    [Siddhi]Add Line Comparision Program added

---
+++ LineComparisionProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+        }
+    }
+}
